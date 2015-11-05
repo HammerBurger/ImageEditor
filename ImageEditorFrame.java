b8ac08622a56f4155c29eca279908a8727cba4fa
Sergey <sergeypavlov93@gmail.com>
2015-11-05 11:24:25 +0100

    panel added

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -6,10 +6,21 @@ import java.io.*;
 import javax.imageio.*;
 public class ImageEditorFrame extends JFrame{
  public ImageEditorFrame(){
-	 setTitle("Image Editor");
- setDefaultCloseOperation(EXIT_ON_CLOSE);
- setSize(400, 300);
- setVisible(true);
+	 
+	setTitle("Image Editor");
+	ImageEditorPanel panel = new ImageEditorPanel();
+	add(panel);
+	
+	BufferedImage bufferedImage = new BufferedImage(400, 300, BufferedImage.TYPE_INT_RGB);
+    Graphics g = bufferedImage.getGraphics();
+    g.setColor(Color.YELLOW);
+    g.fillOval(10, 10, 380, 280);
+    panel.setImage(bufferedImage);
+	
+	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	this.setSize(400, 300);
+	this.setVisible(true);
+	
  }
 
  public static void main(String[] args)
